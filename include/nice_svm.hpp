4affe0ff5801f30628902d881766a82733f4f1f7
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-23 15:25:24 +0200

    Prepare header

---
+++ include/nice_svm.hpp
@@ -0,0 +1,15 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+// http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef NICE_SVM_HPP
+#define NICE_SVM_HPP
+
+namespace svm {
+
+} //end of namespace svm
+
+#endif
